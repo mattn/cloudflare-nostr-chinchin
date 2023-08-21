ff78864e58cb2e3c53acf439c47a993655453d91
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-08-21 16:25:55 +0900

    first import

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+'use strict'
+
+export interface Env {
+    LINE_NOTIFY_TOKEN: string
+}
+
+import querystring from 'querystring'
+
+function notFound(_request: Request, _env: Env) {
+    return new Response(`Not found`, {
+        status: 404,
+    })
+}
+
+function unsupportedMethod(_request: Request, _env: Env) {
+    return new Response(`Unsupported method`, {
+        status: 400,
+    })
+}
+
+async function doChinchin(request: Request, env: Env): Promise<Response> {
+    const mention: { [name: string]: any } = await request.json()
+    let res = await fetch(`https://api.nostr.wine/search?kind=0&pubkey=${mention.pubkey}`)
+    let data: { [name: string]: any } = await res.json()
+    let user = JSON.parse(data.data[0].content)
+    res = await fetch('https://notify-api.line.me/api/notify', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + env.LINE_NOTIFY_TOKEN,
+        },
+        body: querystring.stringify({
+            imageFullsize: user.picture,
+            imageThumbnail: user.picture,
+            message: `${user.name}\n${mention.content}`,
+        })
+    })
+    console.log(await res.json())
+    return new Response('');
+}
+
+export default {
+    async fetch(
+        request: Request,
+        env: Env): Promise<Response> {
+        const { protocol, pathname } = new URL(request.url)
+
+        if ('https:' !== protocol || 'https' !== request.headers.get('x-forwarded-proto')) {
+            throw new Error('Please use a HTTPS connection.')
+        }
+
+        console.log(`${request.method}: ${request.url}`)
+
+        if (request.method === 'POST') {
+            switch (pathname) {
+                case '/chinchin':
+                    return doChinchin(request, env)
+            }
+            return notFound(request, env)
+        }
+
+        return unsupportedMethod(request, env)
+    },
+}
