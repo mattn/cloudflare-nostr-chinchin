ba3014c460a9b5d19828906271543e648ba72d0f
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-05-03 14:50:20 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,69 @@
-'use strict'
+"use strict";
 
 export interface Env {
-    LINE_NOTIFY_TOKEN: string
+  LINE_NOTIFY_TOKEN: string;
 }
 
-import querystring from 'querystring'
+import querystring from "querystring";
 
 function notFound(_request: Request, _env: Env) {
-    return new Response(`Not found`, {
-        status: 404,
-    })
+  return new Response(`Not found`, {
+    status: 404,
+  });
 }
 
 function unsupportedMethod(_request: Request, _env: Env) {
-    return new Response(`Unsupported method`, {
-        status: 400,
-    })
+  return new Response(`Unsupported method`, {
+    status: 400,
+  });
 }
 
 async function doChinchin(request: Request, env: Env): Promise<Response> {
-    const mention: { [name: string]: any } = await request.json()
-    let res = await fetch(`https://api.nostr.wine/search?kind=0&pubkey=${mention.pubkey}`)
-    let data: { [name: string]: any } = await res.json()
-    let user = JSON.parse(data.data[0].content)
-    res = await fetch('https://notify-api.line.me/api/notify', {
-        method: 'post',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer ' + env.LINE_NOTIFY_TOKEN,
-        },
-        body: querystring.stringify({
-            imageFullsize: user.picture,
-            imageThumbnail: user.picture,
-            message: `${user.name}\n${mention.content}`,
-        })
-    })
-    console.log(await res.json())
-    return new Response('');
+  const mention: { [name: string]: any } = await request.json();
+  let res = await fetch(
+    `https://nostr-nullpoga.compile-error.net/profile/${mention.pubkey}`,
+  );
+  let user: { [name: string]: any } = await res.json();
+  res = await fetch("https://notify-api.line.me/api/notify", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": "Bearer " + env.LINE_NOTIFY_TOKEN,
+    },
+    body: querystring.stringify({
+      imageFullsize: user.picture,
+      imageThumbnail: user.picture,
+      message: `${user.name}\n${mention.content}`,
+    }),
+  });
+  console.log(await res.json());
+  return new Response("");
 }
 
 export default {
-    async fetch(
-        request: Request,
-        env: Env): Promise<Response> {
-        const { protocol, pathname } = new URL(request.url)
+  async fetch(
+    request: Request,
+    env: Env,
+  ): Promise<Response> {
+    const { protocol, pathname } = new URL(request.url);
 
-        if ('https:' !== protocol || 'https' !== request.headers.get('x-forwarded-proto')) {
-            throw new Error('Please use a HTTPS connection.')
-        }
+    if (
+      "https:" !== protocol ||
+      "https" !== request.headers.get("x-forwarded-proto")
+    ) {
+      throw new Error("Please use a HTTPS connection.");
+    }
 
-        console.log(`${request.method}: ${request.url}`)
+    console.log(`${request.method}: ${request.url}`);
 
-        if (request.method === 'POST') {
-            switch (pathname) {
-                case '/chinchin':
-                    return doChinchin(request, env)
-            }
-            return notFound(request, env)
-        }
+    if (request.method === "POST") {
+      switch (pathname) {
+        case "/chinchin":
+          return doChinchin(request, env);
+      }
+      return notFound(request, env);
+    }
 
-        return unsupportedMethod(request, env)
-    },
-}
+    return unsupportedMethod(request, env);
+  },
+};
